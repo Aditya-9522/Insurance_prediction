2109a420006aab01bb18b650230590330f16fa05
Aditya Vardhan <adityavardhanp09@gmail.com>
2026-03-11 12:19:42 +0530

    fourth commit

--- src/prediction.py
+++ src/prediction.py
@@ -1,11 +1,13 @@
 import pickle
 import numpy as np
+import os
 class Insurance_Prediction:
     def __init__(self):
-        with open("..\artifacts\scaler.pkl","rb")as f:
-            self.scaler=pickle.load(f)
-        with open("..\artifacts\model.pkl","rb")as f:
-            self.model=pickle.load(f)
+        artifacts_path = os.path.join(os.path.dirname(__file__), '..', 'artifacts')
+        with open(os.path.join(artifacts_path, 'scaler.pkl'),'rb') as f:
+            self.scaler = pickle.load(f)
+        with open(os.path.join(artifacts_path, 'model.pkl'),'rb') as f:
+            self.model = pickle.load(f)
     def prediction(self,Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs):
         input=np.array([[Age,Annual_Income_LPA,Policy_Term_Years,Sum_Assured_Lakhs]])
         scaled_input=self.scaler.transform(input)
